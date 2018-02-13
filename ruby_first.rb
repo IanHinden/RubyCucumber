b35cb4e2d380cd527d99a26831e01b95db177581
IanHinden <Ian.Hinden@gmail.com>
2018-02-13 00:20:18 -0800

    First assignment - Welcome message

--- ruby_first.rb
+++ ruby_first.rb
@@ -1,4 +1,10 @@
+#Asking for first name
+puts "What is your first name?"
+#Assigning name to variable
+firstName = gets
+#Asking for second name
+puts "What is your last name?"
+lastName = gets
+#Displaying message
+puts "Welcome " + firstName + " " + lastName
 
-print "www.thegarbage.org"
-
-print "Welcome to Ruby"
