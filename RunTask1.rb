5f3f61da06807b317b30ff4a706dbfff9c1657dd
IanHinden <Ian.Hinden@gmail.com>
2018-02-14 00:32:50 -0800

    Completed Task 1 Quiz

---
+++ RunTask1.rb
@@ -0,0 +1,6 @@
+require "./Task1"
+
+obj = Task1.new
+m = obj.AddNumbers(20,100)
+n = obj.SubtractNumbers(20,10)
+obj.MultiplyNumbers(m,n)
---
+++ Task1.rb
@@ -0,0 +1,17 @@
+class Task1
+
+	def AddNumbers(a,b)
+		c = a+b
+		return c
+	end
+	
+	def SubtractNumbers(a,b)
+		c=a-b
+		return c
+	end
+
+	def MultiplyNumbers(a,b)
+		c=a*b
+		puts c
+	end
+end
