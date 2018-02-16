f646b4efaaa4dc45481a26c7fad79de59d99e9a4
IanHinden <Ian.Hinden@gmail.com>
2018-02-16 01:03:35 -0800

    Completed conditionals section

--- Call_Condition.rb
+++ Call_Condition.rb
@@ -1,6 +1,7 @@
 require './Condition_1'
 
 obj = Conditions.new
-obj.check_correct_number(22)
-obj.check_correct_number(51)
-obj.check_correct_number(3)
+obj.check_grade(-10)
+obj.check_grade(20)
+obj.check_grade(60)
+obj.check_grade(70)
--- Condition_1.rb
+++ Condition_1.rb
@@ -1,15 +1,24 @@
 =begin
 
 Task : We need to create a method which will take 1 argument
-	If number is greater than 50 or number is even display "Correct Number"
+	If number > 100 or < 1 display "invalid marks"
+	If number between 1-30 failed
+	If number between 31 - 60 passed
+	if number between 61 - 100 Passed with honors.
 	
 =end
 
 class Conditions
 
-	def check_correct_number(a)
-		if(a>50 || a%2==0)
-			puts "Correct number"
+	def check_grade(a)
+		if(a<1 || a>100)
+			puts "Invalid marks"
+		elsif(a>=1 && a<=30)
+			puts "Failed"
+		elsif(a>=31 && a<=60)
+			puts "Passed"
+		else
+			puts "Passed with Honors"
 		end
 	end
 end
