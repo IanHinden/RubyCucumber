fe747d54c567457f7f2951926d2ca666e7ce1ee0
IanHinden <Ian.Hinden@gmail.com>
2018-02-16 13:45:30 -0800

    Completed next keyword exercise

--- ForLoop.rb
+++ ForLoop.rb
@@ -1,10 +1,14 @@
+=begin
+	If number is divisble by 5, don't display. But display remaining umbers
+=end
+
 class Looping
 
 	def generateTable(number)
 	
 		for i in 1..10
 			if i*number%5==0
-				break
+				next
 			else
 				puts i*number
 			end
