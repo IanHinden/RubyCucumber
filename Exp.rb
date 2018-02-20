9c15926335c592fb115f203988386921109b8fcb
IanHinden <Ian.Hinden@gmail.com>
2018-02-19 22:07:28 -0800

    Completed multiple exception exercise

--- Exp.rb
+++ Exp.rb
@@ -1,10 +1,15 @@
 
 begin
+	file=File.open("./notafile.text", 'r')
+	file.readline()
 	a=10
 	b=0
 	c=a/b
 	puts c
-rescue
+rescue ZeroDivisionError => z  
 	puts "This is Exception Handling Block"
+
+rescue 
+	puts "This is the other exception"
 end
 puts "This is the End"
