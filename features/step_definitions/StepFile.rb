cbbdd960f840b03054821cf99e44787b729d43e8
IanHinden <Ian.Hinden@gmail.com>
2018-03-14 22:22:01 -0700

    Added step definitions

---
+++ features/step_definitions/StepFile.rb
@@ -0,0 +1,11 @@
+Given("User is on front page") do
+  puts "User is on front page"
+end
+
+When("user clicks the terms and conditions radio button") do
+  puts "User clicks on radio button"
+end
+
+Then("Terms and Conditions text should appear") do
+  puts "Terms and Conditions appeared"
+end
